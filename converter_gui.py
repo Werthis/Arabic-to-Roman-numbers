878de56819e3c1fdc0a1ed6f042c8ab042d5c276
Werthis <lukaszstawecki@gmail.com>
2021-03-22 10:24:31 +0100

    upload files

---
+++ converter_gui.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import arabic_to_roman_numberals_converter as trans
+
+class GuiTrans():
+
+    def __init__(self, backend):
+        self._backend = backend
+
+        self.window = tk.Tk()
+        self.window.title('Arabic to Roman number conversion')
+        self.window.geometry("300x200")
+
+        self.number = tk.StringVar()
+        self.text = tk.StringVar()
+
+        introduction_label = tk.Label(self.window, text = 'Arabic to Roman number converter', font=('calibre', 12))
+
+        arabic_label = tk.Label(self.window, text = 'enter Arabic number:', font=('calibre', 10))
+
+        input_area = tk.Entry(self.window, textvariable = self.number, font=('calibre',10,'normal'))
+
+        conversion_button = tk.Button(self.window, text='Convert', font=('calibre', 10), command = self.conversion)
+    
+        roman_label = tk.Label(self.window, text = 'This is your roman number:', font=('calibre', 10))
+
+        blanc_label = tk.Label(self.window, text = '')
+
+        roman_number_converted = tk.Label(self.window, textvariable=self.text)
+    
+    
+        # introduction_label.grid(row = 0, column = 1)
+        # arabic_label.grid(row = 2, column = 0)
+        # input_area.grid(row = 2, column = 1)
+        # conversion_button.grid(row = 3, column = 1)
+        # roman_label.grid(row = 5, column = 0)
+        # roman_number_converted.grid(row = 5, column = 1)
+
+        introduction_label.pack(fill=tk.BOTH)
+        arabic_label.pack(fill=tk.BOTH)
+        input_area.pack(fill=tk.BOTH)
+        conversion_button.pack(fill=tk.BOTH)
+        roman_label.pack(fill=tk.BOTH)
+        blanc_label.pack(fill=tk.BOTH)
+        roman_number_converted.pack(fill=tk.BOTH)
+
+
+    def get_number_from_user(self):
+        self.arabic_number = str(0)
+        self.arabic_number = self.number.get()
+        return self.arabic_number
+
+    def conversion(self):
+        self.text.set('')
+        self.get_number_from_user()
+        roman_number = self._backend.input_send_to_gui(self.arabic_number)
+        self.arabic_number = 0
+        self.text.set(roman_number)
+        self.number.set('')
+
+    def start(self):
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    number = ''                            
+    program = trans.Converter(number)
+    app = GuiTrans(program)
+    app.start()
---
+++ test_converter.py
@@ -0,0 +1,118 @@
+import unittest
+from arabic_to_roman_numberals_converter import Converter
+
+class TestMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.number = 89
+        self.program = Converter(self.number)
+
+    def test_make_list_method(self):
+        self.assertEqual(self.program.split_number_and_put_into_list(), [8, 9])
+
+    def test_first_symbol_method(self):
+        string_number = str(self.number)
+        self.assertEqual(self.program.make_first_symbol(int(string_number[1])), 'IX')
+        self.assertEqual(self.program.make_first_symbol(int(string_number[0])), 'VIII')
+
+    def test_second_symbol_method(self):
+        string_number = str(self.number)
+        self.assertEqual(self.program.make_second_symbol(int(string_number[1])), 'XC')
+        self.assertEqual(self.program.make_second_symbol(int(string_number[0])), 'LXXX')
+
+    def test_third_symbol_method(self):
+        string_number = str(self.number)
+        self.assertEqual(self.program.make_third_symbol(int(string_number[1])), 'CM')
+        self.assertEqual(self.program.make_third_symbol(int(string_number[0])), 'DCCC')
+
+    def test_forth_symbol_method(self):
+        self.number = 13
+        self.program = Converter(self.number)
+        string_number = str(self.number)
+        self.assertEqual(self.program.make_forth_symbol(int(string_number[1])), 'MMM')
+        self.assertEqual(self.program.make_forth_symbol(int(string_number[0])), 'M')
+
+    def test_combine_symbols(self):
+        self.program.split_number_and_put_into_list()
+        self.assertEqual(self.program.combine_roman_symbols_to_make_a_number(), 'LXXXIX')
+
+
+class TestNumbers(unittest.TestCase):
+
+    def test_first_ten(self):
+        ten = Converter(3)
+        ten.split_number_and_put_into_list()
+        self.assertEqual(ten.combine_roman_symbols_to_make_a_number(), 'III')
+        ten = Converter(4)
+        ten.split_number_and_put_into_list()
+        self.assertEqual(ten.combine_roman_symbols_to_make_a_number(), 'IV')
+        ten = Converter(6)
+        ten.split_number_and_put_into_list()
+        self.assertEqual(ten.combine_roman_symbols_to_make_a_number(), 'VI')
+        ten = Converter(8)
+        ten.split_number_and_put_into_list()
+        self.assertEqual(ten.combine_roman_symbols_to_make_a_number(), 'VIII')
+
+    def test_till_thousnad(self):
+        hundred = Converter(100)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'C')
+        hundred = Converter(500)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'D')
+        hundred = Converter(210)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'CCX')
+        hundred = Converter(8)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'VIII')
+        hundred = Converter(2)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'II')
+        hundred = Converter(16)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'XVI')
+        hundred = Converter(7)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'VII')
+        hundred = Converter(499)
+        hundred.split_number_and_put_into_list()
+        self.assertEqual(hundred.combine_roman_symbols_to_make_a_number(), 'CDXCIX')
+
+    def test_till_four_thousand(self):
+        thousand = Converter(1000)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'M')
+        thousand = Converter(2000)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'MM')
+        thousand = Converter(3333)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'MMMCCCXXXIII')
+        thousand = Converter(1111)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'MCXI')
+        thousand = Converter(0)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), '')
+        thousand = Converter(1500)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'MD')
+        thousand = Converter(3999)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'MMMCMXCIX')
+        thousand = Converter(101)
+        thousand.split_number_and_put_into_list()
+        self.assertEqual(thousand.combine_roman_symbols_to_make_a_number(), 'CI')
+
+    # def test_error(self):
+    #     error = Converter(4000)
+    #     self.assertRaises("\n\t\tI am very sorry, but roman numbers goes only till 3999", error.check_if_in_range())
+        
+        # (error.combine_roman_symbols_to_make_a_number(), ValueError("\n\t\tI am very sorry, but roman numbers goes only till 3999"))
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
