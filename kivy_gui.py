86eea25a0d816895c71ffed380b78e8e2c882744
Werthis <lukaszstawecki@gmail.com>
2021-04-12 19:57:12 +0200

    VIP #1

--- kivy_gui.py
+++ kivy_gui.py
@@ -5,20 +5,28 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty
 
 import arabic_to_roman_numberals_converter as trans
 
 
 class RomanGrid(Widget):
     arabic_number = ObjectProperty(None)
-    roman_number = ObjectProperty(None)
+    roman_number = StringProperty()
 
-    def __init__(self, backend):
-        self._backend = backend
+    def __init__(self, **kwargs):
+        super(RomanGrid, self).__init__(**kwargs)
+        self.roman_number = 'Here comes your number!'
+
+    def change_text(self):
+        new_number = self.arabic_number.text
+        self.roman_number = str(new_number)
+        self.arabic_number.text = ''
 
     def get_number_from_user(self):
         self.number_to_convert = self.arabic_number.text
+        self.number_to_convert = str(self.number_to_convert)
+        self.roman_number.set(self.number_to_convert)
         print('Arabic number is:', self.number_to_convert)
         self.arabic_number.text = ''
         return self.number_to_convert
@@ -31,9 +39,13 @@ class RomanApp(App):
     def build(self):
         return RomanGrid()
 
+    def start_app(self):
+        RomanApp().run()                
+
 
 if __name__ == '__main__':
     number = ''
     program = trans.Converter(number)
     ## Tu pisz dalej
-    RomanApp().run()
+    app = RomanApp()
+    app.start_app()
